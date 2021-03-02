59a632fb37628ab56010afb9eaf91c271590ee92
pendi <pendi@kemana.com>
2021-03-02 18:29:52 +0700

    add Counter title and add button

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Test Assignment for understanding React JS
+
+This project created only for learing React JS
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/components/navbar.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  render() {
+    const { onAdd, counters } = this.props;
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              Counter App{" "}
+              <button onClick={onAdd} className="btn btn-primary btn-sm m-2">
+                Add Counter
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
